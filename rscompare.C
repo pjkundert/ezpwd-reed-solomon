e38a501b887cdaf942f1687c9422c71af2a5c80c
Perry Kundert <perry@hardconsulting.com>
2015-02-24 09:02:35 -0700

    Minor change in __RS macros; improve rscompare to test accross PARITY

--- rscompare.C
+++ rscompare.C
@@ -7,7 +7,6 @@
 #include <ezpwd/rs>
 #include <ezpwd/timeofday>
 
-#include <ezpwd/definitions>	// must be included in one C++ compilation unit
 
 // Phil Karn's implementation
 extern "C" {
@@ -23,48 +22,55 @@ extern "C" {
 // Use 32 roots and the CCSDS initialization terms, for compatibility with Phil's fastest
 // en/decode_rs_8 fixed-size decoder.
 #define TOTAL	255
-#define ROOTS	32
 #define POLY	0x187
 #define FCR	112
 #define PRIM	11
 
-int
-main()
+template <size_t ROOTS>
+double 				compare()
 {
     std::array<uint8_t,TOTAL>	orig;
 
-    std::cout << "RS(" << TOTAL << "," << (TOTAL-ROOTS) << ") data:" << std::endl;
+    std::cout << std::endl << "RS(" << TOTAL << "," << (TOTAL-ROOTS) << ") w/ " << ROOTS << " parity:" << std::endl;
     for ( size_t i = 0; i < (TOTAL-ROOTS); ++i ) {
 	orig[i]				= i;
+#if defined( DEBUG )
 	if ( i and ( i % 32 == 31 or i == (TOTAL-ROOTS)-1 ))
 	    std::cout << std::vector<uint8_t>( orig.begin() + i - i % 32, orig.begin() + i + 1 ) << std::endl;
+#endif
     }
-
     // Ensure that each RS encoder produces the same parity symbols; both Phil Karn's generic codec,
     // his CCSDS-specific implementation, and the new ezpwd/reed_solomon codec.
     void		       *grs	= init_rs_char( 8, POLY, FCR, PRIM, ROOTS, 0 );
     encode_rs_char( grs, orig.data(), orig.data() + (TOTAL-ROOTS) );
     std::array<uint8_t,TOTAL>	gdata( orig );
+#if defined( DEBUG )
     std::cout
         << "Phil Karn's Generic     parity: "
 	<< std::vector<uint8_t>( gdata.data() + (TOTAL-ROOTS), gdata.data() + TOTAL ) 
         << std::endl;
-
+#endif
     std::array<uint8_t,TOTAL>	cdata( orig );
+if ( ROOTS == 32 ) {
     encode_rs_8( cdata.data(), cdata.data() + (TOTAL-ROOTS), 0 );
+#if defined( DEBUG )
     std::cout
-        << "Phil Karn's CCSDS       parity: "
+	<< "Phil Karn's CCSDS       parity: "
 	<< std::vector<uint8_t>( cdata.data() + (TOTAL-ROOTS), cdata.data() + TOTAL ) 
-        << std::endl;
+	<< std::endl;
+#endif
+}
     
-    ezpwd::RS_CCSDS<TOTAL,TOTAL-ROOTS>
+    static const ezpwd::RS_CCSDS<TOTAL,TOTAL-ROOTS>
 				nrs;
     std::array<uint8_t,TOTAL>	ndata( orig );
     nrs.encode( ndata.data(), nrs.LOAD, ndata.data() + nrs.LOAD );
+#if defined( DEBUG )
     std::cout
         << "EZPWD   " << nrs
 	<< "     parity: " << std::vector<uint8_t>( ndata.data() + (TOTAL-ROOTS), ndata.data() + TOTAL ) 
         << std::endl;
+#endif
     if ( gdata != ndata )
 	throw std::logic_error( "EZPWD and Karn R-S decoders produced different parity" );
 
@@ -110,6 +116,7 @@ main()
     srs_encoder.encode( sdata_str, block );
     std::string			spari_str( fec_length, 0 );
     block.fec_to_string( spari_str );
+#if defined( DEBUG )
     std::cout
         << "Schifra RS("
 	<< (int)schifra::reed_solomon::encoder<code_length,fec_length>::trait::code_length << ","
@@ -117,6 +124,7 @@ main()
 	<< (int)schifra::reed_solomon::encoder<code_length,fec_length>::trait::fec_length  << ")"
 	<< "  parity: " << std::vector<uint8_t>( spari_str.begin(), spari_str.end() ) 
         << std::endl;
+#endif
     // Schifra parity symbols will differ; I can't figure out how to configure non-default
     // polynomial root indices...  Copy the computed data and parity back to sdata from block.
     for ( size_t i = 0; i < sdata.size(); ++i )
@@ -155,6 +163,7 @@ main()
     if ( gdata != orig )
 	throw std::logic_error( "Phil's Generic R-S decoder produced different results" );
 
+if ( ROOTS == 32 ) {
     int				ceras[ROOTS];
     int				ccorrs	= 0;
     double			ctps	= 0;
@@ -186,6 +195,7 @@ main()
         << std::endl;
     if ( cdata != orig )
 	throw std::logic_error( "Phil's CCSDS R-S decoder produced different results" );
+}
 
     // We'll cheat a bit with the Schifra R-S decoder to give it a fighting chance.  Instead of
     // decoding the R-S codeword in-place (in the client's data buffer) like the Phil Karn and EZCOD
@@ -275,4 +285,22 @@ main()
 	throw std::logic_error( "EZPWD R-S decoder produced different results" );
 
     free_rs_char( grs );
+    return ( ntps - gtps ) / gtps * 100;
+}
+
+int main()
+{
+    double			avg	= 0;
+    int				cnt	= 0;
+    avg				       += compare<128>();	++cnt;
+    avg				       += compare< 99>();	++cnt;
+    avg				       += compare< 64>();	++cnt;
+    avg				       += compare< 32>();	++cnt;
+    avg				       += compare< 16>();	++cnt;
+    avg				       += compare< 13>();	++cnt;
+    avg				       += compare<  8>();	++cnt;
+    avg				       += compare<  4>();	++cnt;
+    avg				       += compare<  2>();	++cnt;
+
+    std::cout << std::endl << "RS(255,...) EZPWD vs. Phil Karn's: " << avg/cnt << "% faster (avg.)" << std::endl;
 }
