2ae1c6046fa12358ae97c36f5322862a3df9af33
Perry Kundert <perry@hardconsulting.com>
2022-03-31 10:56:31 -0600

    Correctly denote RS/_CCSDS/_CONV

--- rsexercise.C
+++ rsexercise.C
@@ -15,11 +15,13 @@
 int main() 
 {
     exercise( ezpwd::RS<255,255-2>(), 100 );
-    exercise( ezpwd::RS_CCSDS<255,255-2>(), 100 );
-    exercise( ezpwd::RS_CCSDS<255,255-4>(), 100 );
-    exercise( ezpwd::RS_CCSDS<255,255-5>(), 100 );
-    exercise( ezpwd::RS_CCSDS<255,255-8>(), 100 );
-    exercise( ezpwd::RS_CCSDS<255,255-16>(), 100 );
+    exercise( ezpwd::RS<255,255-4>(), 100 );
+    exercise( ezpwd::RS<255,255-5>(), 100 );
+    exercise( ezpwd::RS<255,255-8>(), 100 );
+    exercise( ezpwd::RS<255,255-16>(), 100 );
+    exercise( ezpwd::RS<255,255-32>(), 100 );
+    exercise( ezpwd::RS_CCSDS<255,255-32>(), 100 );
+    exercise( ezpwd::RS_CCSDS_CONV<255,255-32>(), 100 );
     exercise( ezpwd::RS<511,511-32>(), 10 );
     exercise( ezpwd::RS<1023,1023-32>(), 10 );
     exercise( ezpwd::RS<65535,65535-32>(), 2 );
